086d54dee873f2a3c4ba016ad4e4cc6c7f160173
pawelciupka <pawelciupka@gmail.com>
2023-04-24 22:00:03 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public linkToDrive: string = 'https://drive.google.com/drive/folders/1mS0DCnztqA1Aru3s2IyDPWggJ8eM7cqu?usp=sharing';
 }
